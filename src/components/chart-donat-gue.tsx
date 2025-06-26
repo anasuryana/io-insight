27f98fd854734dab5e819304ecb1c4859d32e04d
Ana Suryana <anasuryana10@gmail.com>
2025-06-26 22:39:55 +0700

    fix : N/A caption

--- src/components/chart-donat-gue.tsx
+++ src/components/chart-donat-gue.tsx
@@ -52,28 +52,28 @@ export default function ChartDonatGue({ theData, lineData }: any) {
     return (
         <Card className="flex flex-col flex-grow h-full min-h-[240px]">
             <CardContent className="flex-grow p-0 min-h-0 overflow-hidden">
-                <div className="w-full h-full min-h-[240px]">
-                    {lineData.is_data_exist == '1' ? (
-                        <ChartContainer
-                            config={chartConfig}
-                            className="w-full h-full"
-                        >
-                            <PieChart>
-                                <ChartTooltip
-                                    cursor={false}
-                                    content={<ChartTooltipContent hideLabel />}
-                                />
-                                <Pie
-                                    data={chartData}
-                                    dataKey="visitors"
-                                    nameKey="browser"
-                                    innerRadius={60}
-                                />
-                                <ChartLegend content={<ChartLegendContent />} />
-                            </PieChart>
-                        </ChartContainer>
-                    ) : (
-                        <div className="w-full h-full flex items-center justify-center">
+                <div className="w-full h-full min-h-[240px] relative">
+
+                    <ChartContainer
+                        config={chartConfig}
+                        className="w-full h-full"
+                    >
+                        <PieChart>
+                            <ChartTooltip
+                                cursor={false}
+                                content={<ChartTooltipContent hideLabel />}
+                            />
+                            <Pie
+                                data={chartData}
+                                dataKey="visitors"
+                                nameKey="browser"
+                                innerRadius={60}
+                            />
+                            <ChartLegend content={<ChartLegendContent />} />
+                        </PieChart>
+                    </ChartContainer>
+                    {lineData.is_data_exist !== '1' && (
+                        <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center pointer-events-none">
                             <span className="text-6xl font-semibold text-gray-500">N/A</span>
                         </div>
                     )}
--- src/components/chart-gue.tsx
+++ src/components/chart-gue.tsx
@@ -44,36 +44,38 @@ export default function ChartGue({ theData, lineData }: any) {
       <CardContent className="flex-grow p-0 min-h-0 overflow-hidden">
 
         {/* Wrapper chart: pakai min-h agar tetap muncul di mobile */}
-        <div className="w-full h-full min-h-[240px]">
-          {lineData.is_data_exist == '1' ? (
-            <ChartContainer
-              config={chartConfig}
-              className="w-full h-full"
+        <div className="w-full h-full min-h-[240px] relative">
+
+          <ChartContainer
+            config={chartConfig}
+            className="w-full h-full"
+          >
+            <BarChart
+              accessibilityLayer
+              data={theData}
+              margin={{ top: 10, right: 10, left: 0, bottom: 30 }}
             >
-              <BarChart
-                accessibilityLayer
-                data={theData}
-                margin={{ top: 10, right: 10, left: 0, bottom: 30 }}
-              >
-                <CartesianGrid vertical={false} />
-                <XAxis
-                  dataKey="time"
-                  tickLine={true}
-                  tickMargin={10}
-                  axisLine={false}
-                  tickFormatter={(value) => value.substring(0, 7)}
-                  textAnchor="end"
-                  interval={0}
-                  className="text-[10px] sm:text-xs"
-                />
-                <YAxis axisLine={false} />
-                <ChartTooltip cursor={false} content={<ChartTooltipContent indicator="dashed" />} />
-                <ChartLegend content={<ChartLegendContent />} />
-                <Bar dataKey="ng" fill="var(--color-ng)" radius={4} />
-                <Bar dataKey="retry" fill="var(--color-retry)" radius={4} />
-              </BarChart>
-            </ChartContainer>) : (
-            <div className="w-full h-full flex items-center justify-center">
+              <CartesianGrid vertical={false} />
+              <XAxis
+                dataKey="time"
+                tickLine={true}
+                tickMargin={10}
+                axisLine={false}
+                tickFormatter={(value) => value.substring(0, 7)}
+                textAnchor="end"
+                interval={0}
+                className="text-[10px] sm:text-xs"
+              />
+              <YAxis axisLine={false} />
+              <ChartTooltip cursor={false} content={<ChartTooltipContent indicator="dashed" />} />
+              <ChartLegend content={<ChartLegendContent />} />
+              <Bar dataKey="ng" fill="var(--color-ng)" radius={4} />
+              <Bar dataKey="retry" fill="var(--color-retry)" radius={4} />
+            </BarChart>
+          </ChartContainer>
+
+          {lineData.is_data_exist !== '1' && (
+            <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center pointer-events-none">
               <span className="text-6xl font-semibold text-gray-500">N/A</span>
             </div>
           )}
