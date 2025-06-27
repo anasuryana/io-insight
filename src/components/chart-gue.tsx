e423ad32d0509b7bef13868f530d6af945e1b733
GilangFauzi7 <112359950+GilangFauzi7@users.noreply.github.com>
2025-06-27 12:29:22 +0700

    fix : auto scale chart Y Axis

--- src/components/chart-gue.tsx
+++ src/components/chart-gue.tsx
@@ -37,7 +37,11 @@ const chartConfig = {
 } satisfies ChartConfig
 
 export default function ChartGue({ theData, lineData }: any) {
-
+  const maxY = Math.max(
+    ...theData.map((item: { ng: number; retry: number }) =>
+      Math.max(item.ng ?? 0, item.retry ?? 0)
+    )
+  )
   return (
     // Card utama: fleksibel + tinggi minimum biar aman di layar kecil
     <Card className="flex flex-col flex-grow h-full min-h-[240px]">
@@ -53,7 +57,7 @@ export default function ChartGue({ theData, lineData }: any) {
             <BarChart
               accessibilityLayer
               data={theData}
-              margin={{ top: 10, right: 10, left: 0, bottom: 30 }}
+              margin={{ top: 30, right: 10, left: 0, bottom: 0 }}
             >
               <CartesianGrid vertical={false} />
               <XAxis
@@ -66,14 +70,15 @@ export default function ChartGue({ theData, lineData }: any) {
                 interval={0}
                 className="text-[10px] sm:text-xs"
               />
-              <YAxis axisLine={false} />
+              <YAxis 
+                axisLine={false}
+                domain={[0, maxY]}/>
               <ChartTooltip cursor={false} content={<ChartTooltipContent indicator="dashed" />} />
               <ChartLegend content={<ChartLegendContent />} />
               <Bar dataKey="ng" fill="var(--color-ng)" radius={4} />
               <Bar dataKey="retry" fill="var(--color-retry)" radius={4} />
             </BarChart>
           </ChartContainer>
-
           {lineData.isDataExist != '1' && (
             <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center pointer-events-none">
               <span className="text-6xl font-semibold text-gray-500">N/A</span>
