13ada33e0cd7c29d197dac1781730d70ac67e468
GilangFauzi7 <gilangfauzi72@gmail.com>
2025-06-30 15:16:30 +0700

    fix : change dialog header name

--- src/app/master/SMSEditDialog.tsx
+++ src/app/master/SMSEditDialog.tsx
@@ -86,7 +86,7 @@ export default function SMSEditDialog({ open, onClose, selectedRowData }: FindMo
         <Dialog open={open} onOpenChange={onClose}>
             <DialogContent className="sm:max-w-2xl">
                 <DialogHeader>
-                    <DialogTitle>Edit Device</DialogTitle>
+                    <DialogTitle>Edit SMS Account</DialogTitle>
                 </DialogHeader>
                 <div className="grid grid-cols-1 md:grid-cols-2 m-2 gap-2">
                     <div>
